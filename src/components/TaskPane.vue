d77ca8a90dccf65258dd0b267f71584c3b961f5c
Williamyz123 <1326447371@qq.com>
2023-02-24 02:16:23 +0800

    加了一个下拉框
    目的是为了日后能针对不同领域进行更准确的推送

--- src/components/TaskPane.vue
+++ src/components/TaskPane.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <el-select v-model="value" placeholder="请选择">
+      <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+      </el-option>
+    </el-select>
     <basic-search></basic-search>
     <DocItem></DocItem>
   </div>
@@ -17,8 +25,23 @@ export default {
   components: {DocItem, BasicSearch},
   data() {
     return {
-      DemoSpan: '',
-      docName: ''
+      options: [{
+        value: '选项1',
+        label: '计算机'
+      }, {
+        value: '选项2',
+        label: '思政'
+      }, {
+        value: '选项3',
+        label: '英语学习'
+      }, {
+        value: '选项4',
+        label: '艺术哲学'
+      }, {
+        value: '选项5',
+        label: '文化历史'
+      }],
+      value: ''
     }
   },
   methods: {
@@ -39,5 +62,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.el-dropdown-link {
+  cursor: pointer;
+  color: #1ca7de;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
 
 </style>
